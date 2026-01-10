74a60d3f6969316ae2a4ab2ad9f42f69098ec519
Daniel Pleşa <plesa.dani@gmail.com>
2026-01-10 18:51:02 +0100

    Change page title from 'alana goyal' to 'daniel plesa'

--- app/notes/[slug]/page.tsx
+++ app/notes/[slug]/page.tsx
@@ -50,7 +50,7 @@ export async function generateMetadata({
   const emoji = note.emoji || "👋🏼";
 
   return {
-    title: `alana goyal | ${title}`,
+    title: `daniel plesa | ${title}`,
     openGraph: {
       images: [
         `/notes/api/og/?title=${encodeURIComponent(title)}&emoji=${encodeURIComponent(
